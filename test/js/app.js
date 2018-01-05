26b263da785b1fcde4048b2bf1bdeaa444e40c61
Jonathan Ohueri <jonathan.ohueri@gmail.com>
2018-01-05 14:14:43 -0500

    moving files from lab to personal

---
+++ test/js/app.js
@@ -0,0 +1,8 @@
+describe('addTwoNumbers', function() {
+    it('returns 5 when passed 2, 3 as values', function() {
+        expect(addTwoNumbers(2, 3)).toEqual(5);
+    });
+     it('returns 11 when passed 5, 6 as values', function() {
+        expect(addTwoNumbers(5, 6)).toEqual(11);
+    });
+});
